d84dbab26f6a17673fd77f56a784f1f6d8c46722
Mitesh Metha <miteshmetha@Miteshs-MacBook-Pro.local>
2023-12-09 08:39:30 +0530

    Added ChainLinkCCIP Dpositor

---
+++ contract/src/Test.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Test is ERC20, Ownable {
+    constructor()
+        ERC20("LINK", "LINK")
+    {
+        _mint(msg.sender, 1000000000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
